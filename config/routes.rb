1eb9c048dd0ef98afe355fe4f404acb02cc0198e
philipp kern <philippk8@gmail.com>
2023-07-20 13:32:58 +0200

    standard navigation added: List, Scan, Generate

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root to: 'basic_qr_codes#index'
   get 'basic-qr-code-reader', to: 'basic_qr_codes#index'
 
-  get '/qrcodes/:id', to: 'qrcodes#show'
+
+  get '/qrcodes/:id(.:format)', to: 'qrcodes#show', as: 'qrcode'
+
 end
