77041099f844cf5269f9f81da44bc58ec2756bfe
philipp kern <philippk8@gmail.com>
2023-07-20 19:54:26 +0200

    Lists added to the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'basic_qr_codes#index'
   get 'basic-qr-code-reader', to: 'basic_qr_codes#index'
+  get 'list', to: 'lists#index'
 
 
   get '/qrcodes/:id(.:format)', to: 'qrcodes#show', as: 'qrcode'
